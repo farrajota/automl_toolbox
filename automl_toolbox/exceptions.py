30e99967237136868c44a6658eff8b5f3fbdfd64
miguel <mglfarrajota@outlook.com>
2018-10-02 17:11:32 +0100

    Add custom exceptions

--- automl_toolbox/exceptions.py
+++ automl_toolbox/exceptions.py
@@ -6,3 +6,13 @@ Exceptions classes.
 class UndefinedMethodError(Exception):
     """Raised if a method is not defined."""
     pass
+
+
+class ModelBackendError(Exception):
+    """Raised if a backend is not defined."""
+    pass
+
+
+class InvalidTaskError(Exception):
+    """Raised if a task is not valid."""
+    pass
