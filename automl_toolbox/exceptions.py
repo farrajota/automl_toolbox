30603ee161175c6b8ee009617b0791dc4c9dcb8f
miguel <mglfarrajota@outlook.com>
2018-10-12 14:04:05 +0100

    Add error messages to exceptions

--- automl_toolbox/exceptions.py
+++ automl_toolbox/exceptions.py
@@ -16,3 +16,11 @@ class ModelBackendError(Exception):
 class InvalidTaskError(Exception):
     """Raised if a task is not valid."""
     pass
+
+
+def raise_invalid_task_error(task: str) -> None:
+    raise InvalidTaskError(f"Invalid task: '{task}'.")
+
+
+def raise_invalid_model_backend_error(backend: str) -> None:
+    raise ModelBackendError(f"Invalid backend: '{backend}'")
