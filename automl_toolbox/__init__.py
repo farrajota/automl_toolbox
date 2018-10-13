f11e98b0ab240f53e0e7d4634268a0b5d45bd2d6
miguel <mglfarrajota@outlook.com>
2018-10-13 22:27:51 +0100

    Update package description

--- automl_toolbox/__init__.py
+++ automl_toolbox/__init__.py
@@ -1,11 +1,24 @@
 """
-ADA: AutoML toolbox
-===================
+AutoML Toolbox
+==============
 
-ADA (Automatic Data Assistant) is a framework for building
-automatic Data Science solutions. Use it as your personal
-Data Science assistant / wizard for creating ETL processes
-and/or a fully automated machine learning pipelines with ease.
+The Automatic Machine Learning toolbox (AutoML) is a collection of methods
+with a simple API which can assist you in your Data Science tasks by providing
+boilerplate code in a form of simple functions ready to be used. You can use
+it as your personal Data Science assistant / wizard for creating ETL processes
+and/or a fully automated machine learning pipelines in a simple and quick way.
+
+This library is intended to be used as a testing playground for a bunch of
+wrapper methods to serve as high-level APIs to a bunch of common tasks like:
+
+- data profiling
+- cleaning missing values
+- detecting outliers
+- performing feature engineering
+- hyper-parameter optimization
+- evaluating machine learning models
+- creating ensembles of such models
+- etc.
 """
 
 
