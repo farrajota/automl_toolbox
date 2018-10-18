cbb137bf3b8d98515b4e32af0258f5ea32b90d22
miguel <mglfarrajota@outlook.com>
2018-10-18 11:25:10 +0100

    Add clustering exception to exceptions

--- automl_toolbox/exceptions.py
+++ automl_toolbox/exceptions.py
@@ -18,9 +18,18 @@ class InvalidTaskError(Exception):
     pass
 
 
+class ClusteringTaskNotImplementedError(Exception):
+    """Raised if a task is clustering (not implemented yet)."""
+    pass
+
+
 def raise_invalid_task_error(task: str) -> None:
     raise InvalidTaskError(f"Invalid task: '{task}'.")
 
 
 def raise_invalid_model_backend_error(backend: str) -> None:
     raise ModelBackendError(f"Invalid backend: '{backend}'")
+
+
+def raise_clustering_not_implemented_error() -> None:
+    raise ClusteringTaskNotImplementedError("Clustering task is not yet implemented.")
